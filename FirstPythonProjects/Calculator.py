ea4f7c48930a09d178d05b436a4869c4398885a4
FerCode <fernandakui28@gmail.com>
2023-02-14 21:22:01 -0300

    Initial

---
+++ FirstPythonProjects/Calculator.py
@@ -0,0 +1,10 @@
+a = int(input('Entre com o primeiro valor'))
+b = int(input('Entre com o segundo valor'))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+
+print('\n SOMA: {}. \n SUBTRACAO: {}. \n MULTIPLICACAO {}. \n DIVISAO {}. \n RESTO {}. \n'.format(soma, subtracao, multiplicacao, divisao, resto))
