6c4998746d961079623104eb5aa3e5171f781fd3
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2023-02-25 00:18:34 +0000

    error

---
+++ FirstPythonProjects/Methods.py
@@ -0,0 +1,12 @@
+class Calculadora:
+    def_init_(self, a, b):
+        self.a = a
+    def soma (a,b):
+        return a + b
+    
+    def sub (a,b):
+        return a - b
+    
+    def mult (a,b):
+        return a * b
+
