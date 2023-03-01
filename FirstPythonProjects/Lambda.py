7855cf7b86f72250dd8fe697ccdafa388f74db2b
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2023-03-01 19:18:55 +0000

    Lambda

---
+++ FirstPythonProjects/Lambda.py
@@ -0,0 +1,13 @@
+
+contador_letras = lambda lista: [len(x) for x in lista]
+
+lista_animais = ['cachorro', 'gato', 'elefante']
+print(contador_letras(lista_animais))
+
+
+#calculadora = {
+ #   'soma': lambda a, b = a + b,
+  #  'subtra' : lambda a, b = a - b,
+   # 'mult' : lambda a, b = a * b,
+    #'div' : lambda a, b = a / b,
+#}
