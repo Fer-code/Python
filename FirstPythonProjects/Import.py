fa9ffd2506893f9f319f0c16990aa8160976f62c
Fernanda Rocha Miranda <fernandamirandacursos@gmail.com>
2023-03-01 18:59:58 +0000

    novos

---
+++ FirstPythonProjects/Import.py
@@ -0,0 +1,15 @@
+from Television import Televisao
+#from Calculator import Calculo
+from Contador_palavras import contador_letras #pode importar classes e métodos tmb
+
+if __name__ == '__main__':
+    TV = Televisao()
+    print(TV.ligada)
+    TV.power()
+    print(TV.ligada)
+
+    lista = ['cachorro' , 'gato' , 'elefante']
+    print(contador_letras(lista))
+
+    #ca = Calculo(1,2)
+    #print(ca.soma())
--- FirstPythonProjects/Television.py
+++ FirstPythonProjects/Television.py
@@ -15,22 +15,23 @@ class Televisao:
     def diminui_canal(self):
         self.canal -= 1
 
-televisao = Televisao()
-print(televisao.ligada)
+if __name__ == '__main__':
+    televisao = Televisao()
+    print(televisao.ligada)
 
-televisao.power()
-print(televisao.ligada)
+    televisao.power()
+    print(televisao.ligada)
 
-televisao.power()
-print(televisao.ligada)
+    televisao.power()
+    print(televisao.ligada)
 
-print(televisao.canal)
-televisao.aumenta_canal()
+    print(televisao.canal)
+    televisao.aumenta_canal()
 
-print(televisao.canal)
-televisao.diminui_canal()
+    print(televisao.canal)
+    televisao.diminui_canal()
 
-print(televisao.canal)
+    print(televisao.canal)
 
 
 
